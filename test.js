ae6c5c82891f3b406191de6e2257f6ae37879741
thomastokmakov24 <thomast89@nycstudents.net>
2025-12-19 12:30:58 -0500

    this is not fun
    not fun

--- test.js
+++ test.js
@@ -1,27 +1,41 @@
-async function testApi() {
-  try {
-    const response = await fetch('https://kanye.rest/');
+// async function testApi() {
+//   try {
+//     const response = await fetch('https://kanye.rest/');
 
-    console.log('Status:', response.status); // 200 = OK
+//     console.log('Status:', response.status); // 200 = OK
 
-    if (!response.ok) {
-      throw new Error('Request failed');
-    }
+//     if (!response.ok) {
+//       throw new Error('Request failed');
+//     }
 // /* 
 //     const data = await response. */const text = await response.text();
 // /* console.log(text); */
 // json();
 //const response = await fetch('https://kanye.rest/');
 
-console.log('content-type:', response.headers.get('content-type'));
+// console.log('content-type:', response.headers.get('content-type'));
 
-const text = await response.text(); // 👈 instead of .json()
-console.log(text);
+// const text = await response.text(); // 👈 instead of .json()
+// console.log(text);
 
-    console.log('API response:', data);
-  } catch (err) {
-    console.error('Error:', err);
-  }
-}
+//     console.log('API response:', data);
+//   } catch (err) {
+//     console.error('Error:', err);
+//   }
+// }
 
-testApi();
+// // testApi();
+// const api = await fetch('https://kanye.rest/');
+// const apidata = api.json();
+// console.log(apidata);
+fetch("https://icanhazdadjoke.com/api")
+  .then(response => {
+    console.log("Response object:", response);
+    return response.json();
+  })
+  .then(data => {
+    console.log("API data:", data);
+  })
+  .catch(error => {
+    console.error("Fetch error:", error);
+  });
